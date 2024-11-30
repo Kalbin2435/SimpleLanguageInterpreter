620c49550f0b6cacb249e51d3773c02036bc2a42
) <kalbin2435@gmail.com>
2024-11-30 20:58:12 +0200

    added few literals to scanner

--- csharplox/lox/Scanner.cs
+++ csharplox/lox/Scanner.cs
@@ -25,44 +25,96 @@ class Scanner {
         char c = advance();
         switch (c)
         {
-            case '(':
-                addToken(TokenType.LEFT_PAREN);
-                break;
-            case ')':
-                addToken(TokenType.RIGHT_PAREN);
-                break;
-            case '{':
-                addToken(TokenType.LEFT_BRACE);
-                break;
-            case '}':
-                addToken(TokenType.RIGHT_BRACE);
-                break;
-            case ',':
-                addToken(TokenType.COMMA);
-                break;
-            case '.':
-                addToken(TokenType.DOT);
-                break;
-            case '-':
-                addToken(TokenType.MINUS);
-                break;
-            case '+':
-                addToken(TokenType.PLUS);
-                break;
-            case ';':
-                addToken(TokenType.SEMICOLON);
-                break;
-            case '*':
-                addToken(TokenType.STAR);
-                break;
+            case '(': addToken(TokenType.LEFT_PAREN); break;
+            case ')': addToken(TokenType.RIGHT_PAREN); break;
+            case '{': addToken(TokenType.LEFT_BRACE); break;
+            case '}': addToken(TokenType.RIGHT_BRACE); break;
+            case ',': addToken(TokenType.COMMA); break;
+            case '.': addToken(TokenType.DOT); break;
+            case '-': addToken(TokenType.MINUS); break;
+            case '+': addToken(TokenType.PLUS); break;
+            case ';': addToken(TokenType.SEMICOLON); break;
+            case '*': addToken(TokenType.STAR); break;
+            case '!': addToken(match('=') ? TokenType.BANG_EQUAL : TokenType.BANG); break;
+            case '=': addToken(match('=') ? TokenType.EQUAL_EQUAL : TokenType.EQUAL); break;
+            case '<': addToken(match('=') ? TokenType.LESS_EQUAL : TokenType.LESS); break;
+            case '>': addToken(match('=') ? TokenType.GREATER_EQUAL : TokenType.GREATER); break;
+            case '"': _string(); break;
+            case '/': slash(); break;
+            case ' ':
+            case '\r':
+            case '\t': 
+                      break;
+            case '\n':
+                  line++;
+                  break;
             default:
-                Lox.error(line, "Unexpected character");
-                break;
-                
+                  if (isDigit(c)) {
+                      number();
+                  } else {
+                      Lox.error(line, "Unexpected character");
+                  }
+                  break;
+
         }
 
     }
 
+    private void _string() {
+        while(peek() != '"' && !isAtEnd()) {
+            if(peek() == '\n') line++;
+            advance();
+        }
+
+        if(isAtEnd()) {
+            Lox.error(line, "Unterminated string.");
+            return;
+        }
+
+        advance();
+        string value = source.Substring(start + 1, current_minus_start() - 1);
+        addToken(TokenType.STRING, value);
+    }
+
+    private void number() {
+        while(isDigit(peek())) advance();
+        if(peek() == '.' && isDigit(peekNext())) {
+            advance();
+            while (isDigit(peek())) advance();
+        }
+
+        addToken(TokenType.NUMBER, Double.Parse(source.Substring(start, current_minus_start())));
+    }
+
+    private void slash() {
+        if(match('/')) {
+            while (peek() != '\n' && !isAtEnd()) advance();
+        } else {
+            addToken(TokenType.SLASH);
+        }
+    }
+
+    private bool isDigit(char c) {
+        return c >= '0' && c <= '9';
+    }
+
+    private char peekNext() {
+        if(current + 1 >= source.Length) return '\0';
+        return source[current+1];
+    }
+
+    private char peek() {
+        if(isAtEnd()) return '\0';
+        return source[current];
+    }
+
+    private bool match(char expected) {
+        if(isAtEnd()) return false;
+        if (source[current] != expected) return false;
+
+        current++;
+        return true;
+    }
 
     private bool isAtEnd() {
         return current >= source.Length;
@@ -78,8 +130,12 @@ class Scanner {
     }
 
     private void addToken(TokenType type, object literal) {
-        int distance_to_end_index = current - start;
-        string text = source.Substring(start, distance_to_end_index);
+        string text = source.Substring(start, current_minus_start());
         tokens.Add(new Token(type, text, literal, line));
+    }
+
+    private int current_minus_start() {
+        int current_minus_start = current - start;
+        return current_minus_start;
     }
 }
